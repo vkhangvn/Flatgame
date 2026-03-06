7e43a4bdc57e5abcd568d9ab0afd2d9906eb53c3
vkhangvn <147364281+vkhangvn@users.noreply.github.com>
2026-03-07 02:26:05 +0700

    ụp

---
+++ Assets/Script/Transition3.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Transition3 : MonoBehaviour
+{
+    public Button button;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+        
+    }
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene("Scene 4");
+
+    }
+    
+}
