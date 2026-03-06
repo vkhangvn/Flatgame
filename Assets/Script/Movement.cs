650de411dacb04dfa570bd237baf391d48ec6f2b
vkhangvn <vkhangvn@gmail.com>
2026-03-06 19:06:55 +0700

    úp 3

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -1,3 +1,4 @@
+using Fungus;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -6,6 +7,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float speed = 10f;
+    public GameObject moveSound;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,19 +20,39 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(0, speed * Time.deltaTime, 0);
+            moveSound.SetActive(true);
         }
         if (Input.GetKey(KeyCode.S))
 
         {
             transform.Translate(0, -speed * Time.deltaTime, 0);
+            moveSound.SetActive(true);
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(speed * Time.deltaTime, 0, 0);
+            moveSound.SetActive(true);
         }
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(-speed * Time.deltaTime, 0, 0);
+            moveSound.SetActive(true);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            moveSound.SetActive(false);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            moveSound.SetActive(false);
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            moveSound.SetActive(false);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            moveSound.SetActive(false);
         }
     }
 }
--- Assets/Script/sceneTriggergeektroad.cs
+++ Assets/Script/sceneTriggergeektroad.cs
@@ -1,28 +1,34 @@
-using Fungus;
 using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Crash : MonoBehaviour
+public class sceneTriggergeektroad : MonoBehaviour
 {
-
-    public GameObject crashScene;
-    public GameObject playerSpawn;
     public GameObject player;
+    public GameObject loadScreen;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
     private void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            crashScene.SetActive(true);
+            GameObject.Destroy(player);
+            loadScreen.SetActive(true);
             StartCoroutine(Timer());
+            
         }
+
     }
     IEnumerator Timer()
     {
         yield return new WaitForSeconds(2.5f);
-        SceneManager.LoadScene("Road to geeky");
+        SceneManager.LoadScene("Scene 2");
     }
-
 }
