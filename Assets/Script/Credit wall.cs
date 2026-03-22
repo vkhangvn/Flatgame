2c24e17394b48a110563e1f70618fb1de8160b05
vkhangvn <147364281+vkhangvn@users.noreply.github.com>
2026-03-22 16:15:40 +0700

    h

---
+++ Assets/Script/Credit wall.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Numerics;
+
+public class Creditwall : MonoBehaviour
+{
+
+    public GameObject crashScene;
+    public GameObject player;
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            crashScene.SetActive(true);
+            StartCoroutine(Timer());
+        }
+    }
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene("Main menu");
+    }
+
+}
