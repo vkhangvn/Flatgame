877199f2031d268c181c89c24cf3ef578ff32047
vkhangvn <vkhangvn@gmail.com>
2026-03-23 17:15:49 +0700

    a

---
+++ Assets/Script/CreditButton.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CreditButton : MonoBehaviour
+{
+    public Button button;
+    public GameObject loadScreen;
+    public GameObject text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void TaskOnClick()
+    {
+
+        StartCoroutine(Timer());
+
+    }
+    IEnumerator Timer()
+    {
+        loadScreen.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene("Credit");
+    }
+}
